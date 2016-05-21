1233419643e15a412241edb83d2dd2e9a375b68f
root <padalavenkatesh786@gmail.com>
2016-05-21 06:51:45 +0000

    arithmatic

---
+++ arithmatic.sh
@@ -0,0 +1,12 @@
+#### WAS which will add, sub, mul, divide the given nos. ###
+
+echo "Please Enter ur first number"
+read a
+echo "Please Enter ur second no"
+read b
+echo "####################"
+echo `expr $a + $b`
+echo `expr $a - $b`
+echo `expr $a \* $b`
+echo `expr $a / $b`
+
